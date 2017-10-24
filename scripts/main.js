ad6777b4b78367b02bc4bddfdd3e64b37a5d38d6
Oluwatobi Awe <awetob@gmail.com>
2017-10-24 02:48:56 +0100

    Added code to remove and add active class on click

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,11 @@ $(document).on('click', '.nav li a', function(event){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
-
-
 });
+
+$('nav li a').on('click', function(event){
+    $(".nav li a").removeClass("active");
+    $(this).addClass("active");
+})
 
 
